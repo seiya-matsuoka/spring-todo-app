9f75d5a6a21d915c1a7798e2bc07d5e2d26af028
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-07-20 22:48:35 +0900

    feat: テーブル構築と初期データ登録に使用するSQLを作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO todos (todo, detail, created_at, updated_at) VALUES ('やること1', '初期データ1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO todos (todo, detail, created_at, updated_at) VALUES ('やること2', '初期データ2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO todos (todo, detail, created_at, updated_at) VALUES ('やること3', '初期データ3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
