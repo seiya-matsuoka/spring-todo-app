c4a8fa820fd849d30ad5ab987eba11ebaeb23109
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-07-27 22:48:14 +0900

    feat: DBアクセス確認用コードをserviceを介した実行へと変更

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.todo.entity.ToDo;
-import com.example.todo.repository.ToDoMapper;
+import com.example.todo.service.ToDoService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,35 +17,35 @@ public class TodoApplication {
 		.getBean(TodoApplication.class).exe();
 	}
 
-	private final ToDoMapper mapper;
+    private final ToDoService service;
 
     public void exe() {
         System.out.println("=== 全件検索 ===");
-        for (ToDo row : mapper.selectAll()) {
+        for (ToDo row : service.findAllToDo()) {
             System.out.println(row);
         }
 
         System.out.println("=== １件検索 ===");
-        System.out.println(mapper.selectById(1));
+        System.out.println(service.findByIdToDo(1));
 
         ToDo todo = new ToDo();
         todo.setTodo("登録テスト");
         todo.setDetail("登録処理");
-        mapper.insert(todo);
+        service.insertToDo(todo);
         System.out.println("=== 登録確認 ===");
-        System.out.println(mapper.selectById(4));
+        System.out.println(service.findByIdToDo(4));
 
-        ToDo target = mapper.selectById(4);
+        ToDo target = service.findByIdToDo(4);
         target.setTodo("更新テスト");
         target.setDetail("更新処理");        
-        mapper.update(target);
+        service.updateToDo(target);
         System.out.println("=== 更新確認 ===");
-        System.out.println(mapper.selectById(4));
+        System.out.println(service.findByIdToDo(4));
 
-        mapper.delete(4);
+        service.deleteToDo(4);
         System.out.println("=== 削除確認 ===");
-        for (ToDo row : mapper.selectAll()) {
+        for (ToDo row : service.findAllToDo()) {
             System.out.println(row);
-        }        
+        }
     }
 }
